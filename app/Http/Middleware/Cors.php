cfbc2ad52d553cf80126c754e2b6ede86370a153
diamanka98 <diamanka1998@gmail.com>
2021-03-02 18:11:09 +0000

    Ajout url application angular

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Origin,Content-Type,Accept, Authorization,X-Resquest')
+        ->header('Access-Control-Allow-Credentials', 'true');
+        
+    }
+}
