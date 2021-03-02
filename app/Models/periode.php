95dae75eeedd6eaeb25f0d81a27baa7ce9e37088
CYC <cheikhyakhouba97@gmail.com>
2021-03-02 11:54:47 +0000

    Commit

--- app/Models/periode.php
+++ app/Models/periode.php
@@ -8,11 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class periode extends Model
 {
     protected $primaryKey = 'idPeriode';
-<<<<<<< HEAD
     protected $fillable = ['nomPeriode','phase'];
-=======
-    public $filable = ['nomPeriode','phase'];
->>>>>>> e0a4ad4cda9138b6bcd878d92f370f6dc62db06d
     use HasFactory;
 
     public function vaches()
--- app/Models/utilisateur.php
+++ app/Models/utilisateur.php
@@ -8,10 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class utilisateur extends Model
 {
     protected $primaryKey = 'idUtilisateur';
-<<<<<<< HEAD
     protected $fillable = ['nom','prenom','tel','adresse','photo','login','password','profile'];
-=======
-    public $filable = ['nom','prenom','tel','adresse','photo','login','password','profile'];
->>>>>>> e0a4ad4cda9138b6bcd878d92f370f6dc62db06d
     use HasFactory;
 }
