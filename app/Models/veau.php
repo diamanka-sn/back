1943e48491c3504b9ccdb3e99b827515aec14e6e
diamanka98 <diamanka1998@gmail.com>
2021-03-28 15:13:59 +0000

    commit

--- app/Models/veau.php
+++ app/Models/veau.php
@@ -8,11 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class veau extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-    protected $fillable = ['codeBovin','idBovin'];
-=======
     protected $fillable = ['idBovin','codeBovin'];
->>>>>>> 764f3b8423924a5e5abea5b7cb9d24c978bfe902
     
     public $incrementing = false;
     protected $primaryKey = 'idBovin';
